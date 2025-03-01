ed552f421520bcdde4c269c020c441e1388c46ad
ahmvdev <ahmadsaeed3290@gmail.com>
2025-03-01 20:36:47 +0500

    added delete and checkbox functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,19 @@ import { useState } from "react";
 function App() {
   const [task, setTask] = useState([]);
 
-  const allTasks = task.map((t) => <li key={t}>{t}</li>);
+  const deleteTask = (formData) => {
+    const taskToDelete = Number(formData.get("index"));
+    const updatedTasks = task.filter((_, index) => index !== taskToDelete);
+    setTask(updatedTasks);
+  };
+
+  const allTasks = task.map((t, index) => (
+    <form className="flex justify-between" key={index} action={deleteTask}>
+      <input type="checkbox" className="mr-5" id={index} />
+      <label id={index}>{t}</label>
+      <button className="pl-5 hover:cursor-pointer">X</button>
+    </form>
+  ));
 
   const addTask = (formData) => {
     const gotInput = formData.get("tasks");
@@ -16,7 +28,7 @@ function App() {
       <form action={addTask}>
         <div className="flex justify-center pt-10">
           <input placeholder="enter your task here" name="tasks"></input>
-          <button>Add Task</button>
+          <button className="hover:cursor-pointer">Add Task</button>
         </div>
       </form>
       <div className="flex flex-col items-center pt-10 ">
